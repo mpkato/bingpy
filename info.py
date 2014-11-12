151f3678694ab2af6fb7f37500c50e8e53027e8b
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2014-11-12 23:10:59 +0900

    add web_search

---
+++ info.py
@@ -0,0 +1,12 @@
+# package information.
+INFO = dict(
+  name        = "bingpy",
+  description = "Python Bing Search API (2014)",
+  author      = "Makoto P. Kato",
+  author_email = "kato@dl.kuis.kyoto-u.ac.jp",
+  license     = "MIT License",
+  url         = "https://github.com/mpkato/bingpy",
+  classifiers = [
+    "Programming Language :: Python :: 2.7"
+  ]
+)
---
+++ lib/bingpy/bingpy/web_search.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+import urllib2
+from lxml import etree
+
+from .web_search_element import WebSearchElement
+
+class WebSearch:
+    APIURL = "https://api.datamarket.azure.com/Bing/Search/v1/Web"
+    XMLNS = "{http://www.w3.org/2005/Atom}"
+    RESULT_MAX = 50
+
+    def __init__(self, apikey):
+        self.apikey = apikey
+
+    def search(self, query, num, market = 'en-US'):
+        # retrieve 'num' Web search results by 'query' from 'market'
+        # compute the number of Web accesses
+        access_num = (num - 1) / self.RESULT_MAX + 1
+        result = []
+        for i in range(access_num):
+            # get RESULT_MAX search results
+            result += self._search_original(query, i * self.RESULT_MAX, market)
+        # truncate (redundant...)
+        result = result[:min(len(result), num)]
+        return result
+
+    def _search_original(self, query, skip, market):
+        url = self._make_url(query, skip, market)
+        self._basic_auth()
+        with urllib2.urlopen(url) as f:
+            yield self._parse_response(f)
+
+    def _make_url(self, query, skip, market):
+        return self.APIURL + "?Query='%s'&Market='%s'&$skip=%s" % (urllib2.quote(query), market, skip)
+
+    def _basic_auth(self):
+        password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+        password_mgr.add_password(None, self.APIURL, self.apikey, self.apikey)
+        handler = urllib2.HTTPBasicAuthHandler(password_mgr)
+        opener = urllib2.build_opener(handler)
+        urllib2.install_opener(opener)
+
+    def _parse_response(self, f):
+        context = etree.iterparse(f, events=('end',))
+        for event, elem in context:
+            if elem.tag == '%sentry' % self.XMLNS:
+                for c in elem:
+                    if c.tag == '%scontent' % self.XMLNS:
+                        elem = WebSearchElement.parse(c[0])
+                        yield elem
