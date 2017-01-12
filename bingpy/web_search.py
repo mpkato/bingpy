538f43e34091953510edf0ebf355b016d81f38b4
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2017-01-12 14:15:51 +0900

    Clean up

--- bingpy/web_search.py
+++ bingpy/web_search.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
-import urllib2
+import requests
 from lxml import etree
 
 from web_search_element import WebSearchElement
 
 class WebSearch:
-    APIURL = "https://api.datamarket.azure.com/Bing/Search/v1/Web"
-    XMLNS = "{http://www.w3.org/2005/Atom}"
+    APIURL = "https://api.cognitive.microsoft.com/bing/v5.0/search"
     RESULT_MAX = 50
 
     def __init__(self, apikey):
         self.apikey = apikey
 
-    def search(self, query, num, market = 'en-US'):
+    def search(self, query, num, market = 'en-US', safesearch='Off'):
         # retrieve 'num' Web search results by 'query' from 'market'
         # compute the number of Web accesses
         access_num = (num - 1) / self.RESULT_MAX + 1
@@ -26,25 +25,20 @@ class WebSearch:
 
     def _search_original(self, query, skip, market):
         url = self._make_url(query, skip, market)
+        header = {"Ocp-Apim-Subscription-Key": self.apikey}
         self._basic_auth()
         f = urllib2.urlopen(url)
         try:
             result = self._parse_response(f)
-        except Exception, e:
+        except Exception as e:
             raise e
         finally:
             f.close()
         return result
 
-    def _make_url(self, query, skip, market):
-        return self.APIURL + "?Query='%s'&Market='%s'&$skip=%s" % (urllib2.quote(query), market, skip)
-
-    def _basic_auth(self):
-        password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
-        password_mgr.add_password(None, self.APIURL, self.apikey, self.apikey)
-        handler = urllib2.HTTPBasicAuthHandler(password_mgr)
-        opener = urllib2.build_opener(handler)
-        urllib2.install_opener(opener)
+    def _make_url(self, query, num, skip, market, safesearch):
+        return (self.APIURL + "?q=%s&count=%s&offset=%s&mkt=%s&safesearch=%s"
+            % (urllib2.quote(query), num, skip, market, safesearch))
 
     def _parse_response(self, f):
         result = []
@@ -56,3 +50,8 @@ class WebSearch:
                         elem = WebSearchElement.parse(c[0])
                         result.append(elem)
         return result
+
+if __name__ == '__main__':
+    import os
+    ws = WebSearch(os.environ["BING_API_KEY_V5"])
+    ws.search("hoge", 100)
--- setup.py
+++ setup.py
@@ -1,27 +1,17 @@
 # -*- coding:utf-8 -*-
-from setuptools import setup, find_packages
-import os
-import sys
+from setuptools import setup
 
-def find_scripts(scripts_path):
-  base_path = os.path.abspath(scripts_path)
-  return list(map(lambda path: os.path.join(scripts_path, path), 
-           filter(lambda file_name: os.path.isfile(
-             os.path.join(base_path, file_name)), os.listdir(base_path)
-         )))
-
-
-libdir = "lib/bingpy"
-sys.path.insert(0, libdir)
-
-import info
-import version
-setup_options = info.INFO
-setup_options["version"] = version.VERSION
-setup_options.update(dict(
-  install_requires = open('requirements.txt').read().splitlines(),
-  packages         = find_packages(libdir),
-  package_dir      = {"": libdir},
-))
-
-setup(**setup_options)
+setup(
+    name = "bingpy",
+    packages = ["bingpy"],
+    version = "0.0.1",
+    description = "Bing Search APIs (V5)",
+    author = "Makoto P. Kato",
+    author_email = "kato@dl.kuis.kyoto-u.ac.jp",
+    license     = "MIT License",
+    url = "https://github.com/mpkato/bingpy",
+    install_requires = [
+        "requests",
+    ],
+    tests_require=['pytest'],
+)
